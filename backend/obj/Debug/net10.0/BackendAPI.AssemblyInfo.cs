9fbde02c288216c6242f2693bcb9e6a6a93fdf0c
solo3li <hamed3alii.3@gmail.com>
2026-06-09 08:09:05 -0700

    chore: generate build artifacts and debug metadata for backend API

--- backend/obj/Debug/net10.0/BackendAPI.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/BackendAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2d08b49c2f10fbb466f18e6a4867be4dd9fbc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a05297caebd39f7fab976728680b63ff2e78b10d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
